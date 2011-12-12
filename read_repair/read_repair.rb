c9f149a3bc8315ad3a5256ae45bc3740c6f3aa53
ray <ray@ray-U33Jc.(none)>
2011-12-12 10:50:00 -0800

    read_repair.rb now takes in a coordinator in the input and increments that coordinator

--- read_repair/read_repair.rb
+++ read_repair/read_repair.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'bud'
-require 'version_vector'
+require 'causality/version_vector'
 
-# How to use: (anything that appears in an input interface, appears in the output interface in one tick)
+# How to use: (anything that appears in an input interface, appears in the output interface in one tick. NO STATE PERSISTS WITHIN THIS MODULE)
 #
 # When the quorum agent has received all of the required read acks, it should pipe those acks into read_acks and the corresponding read request in read_requests
 # This implementation allows several read requests to be handled on the same timestep
@@ -12,27 +12,37 @@ require 'version_vector'
 #
 module ReadRepairProtocol
   state do
+    # NOTE: A read can result in the same readacks among all reads. This corresponds to a read in whic all of the versions are the same
+    # The user is responsible for ensuring that only one instance of duplicate read_acks is inserted into this interface
     interface input, :read_acks, [:request, :v_vector] => [:value]
-    interface input, :read_requests, [:request] => [:key]
     
-    # if acks are not synchronized, write requests will be non-empty. The contents of write requests should be piped into the corresponding write channel
+    # NOTE: read_requests DOES NOT PERSIST INTERNALLY. It is up to the user of this module to cache the key, and coordinator server for a read request.
+    # The user can garbage collect the cached state after it has received all the required read acks and piped it into this interface
+    interface input, :read_requests, [:request] => [:key, :server]
+    
+    # NOTE: this module only returns a single write per request if version conflicts were detected. The quorum layer is responsible for propagating the
+    # write request to the proper nodes
     interface output, :write_requests, [:request] => [:key, :v_vector, :value]
   end
 end
 
 module ReadRepair
   include ReadRepairProtocol
+  include VersionVectorSerializerProtocol
   import VersionVectorConcurrency => :vvc
   import VersionVectorMerge => :vvm
   
   state do
+    # temp state. I wish temps could have schema declarations
     scratch :freshest_reads, [:request, :v_vector] => [:value]
     scratch :merged_values, [:request] => [:value]
     scratch :num_freshest_reads, [:request] => [:size]
+    scratch :incremented_coordinators, [:request, :server] => [:version]
     scratch :pending_write_requests, [:request] => [:key, :v_vector, :value]
   end
 
   bloom :determine_concurrent_clocks do
+    # some glue code
     vvc.version_matrix <= read_acks { |ra| [ra.request, ra.v_vector] }
   end
 
@@ -50,8 +60,36 @@ module ReadRepair
     # merge the clocks of the minimal matrix
     vvm.version_matrix <= freshest_reads {|t| [t.request, t.v_vector]}
 
+    # increment the coordinator in the merged vector clock
+    # is another VV serializer implementation, but this one increments coordinataors
+    deserialize <= vvm.merge_vector
+    deserialize_ack <= deserialize.flat_map do |x|
+      x.v_vector.map do |y|
+        [x.request, y[0], y[1]]
+      end
+    end
+    
+    # increment if element is a coordinator
+    incremented_coordinators <= (deserialize_ack * read_requests).pairs(:request=>:request) do |l,r| 
+      [l.request, l.server, l.version + 1] if l.server == r.server
+    end
+
+    # element stays the same if it is not a coordinator
+    incremented_coordinators <= (deserialize_ack * read_requests).pairs(:request=>:request) do |l,r|
+      l if l.server != r.server
+    end
+    
+    # reserialize the incremented vectors
+    serialize <= incremented_coordinators
+
+    serialize_ack <= serialize.reduce({}) do |meta, x|
+      meta[x.request] ||= []
+      meta[x.request] << [x.server, x.version]
+      meta
+    end
+
     # pending_write_requests contains one write per request id, that is guaranteed to be fresh. That write might be redundant, however
-    pending_write_requests <= (read_requests * merged_values * vvm.merge_vector).matches do |l,m,r|
+    pending_write_requests <= (read_requests * merged_values * serialize_ack).matches do |l,m,r|
       [l.request, l.key, r.v_vector, m.value]
     end
 
