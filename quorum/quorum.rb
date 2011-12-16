2ecf555db42bbf30089955b9fd337109e78107ed
ray <ray@ray-U33Jc.(none)>
2011-12-16 11:26:31 -0800

    timeout logic finished

--- quorum/quorum.rb
+++ quorum/quorum.rb
@@ -240,6 +240,7 @@ module RWTimeoutQuorumAgent
     voter.cast_vote <= qa.write_response {|wr| [wr.request, wr.src, "ack", "no note"]}
   end
 
+  # handling results and timeouts
   bloom do
      # timer runs out, vote fails, output error
     status <= (alarm.alarm * voter.result).pairs(:ident=>:ballot_id) do |l,r|
@@ -251,9 +252,13 @@ module RWTimeoutQuorumAgent
     end
 
     # vote successful, timer still going, clear timer: output success
-=begin
-    status <= vote.result do |r|
-      [r.ballot_id, :success] if 
-=end
+    status <= voter.result do |r|
+      [r.ballot_id, :success] if r.status == :success
+    end
+    
+    alarm.stop_alarm <= vote.result do |r|
+      [r.ballot_id] if r.status == :success
+    end
+    
   end
 end
