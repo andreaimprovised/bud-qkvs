3a04a1b24395686a7c5f725e18ac47839b5bac5a
Alex Degtiar <alex.degtiar@gmail.com>
2011-12-16 14:55:56 -0800

    Adding simple session tests.

--- test/tc_session_voting.rb
+++ test/tc_session_voting.rb
@@ -34,22 +34,50 @@ class TestSessionVoting < Test::Unit::TestCase
     p 'test_init_guarantees'
     init_dummy_request
     wait
-    assert(@voter.session_guarantees.include? [0, [:foo, :bar]])
+    assert(@voter.session_guarantees.include?([0, [:foo, :bar]]))
   end
 
   def test_init_read_vectors
     p 'test_init_read_vectors'
     init_dummy_request
     wait
-    assert(@voter.read_vectors.include? [0, ['a', 0]])
-    assert(@voter.read_vectors.include? [0, ['b', 1]])
+    assert(@voter.read_vectors.include?([0, ['a', 0]]))
+    assert(@voter.read_vectors.include?([0, ['b', 1]]))
   end
 
   def test_init_write_vector
     p 'test_init_write_vectors'
     init_dummy_request
     wait
-    assert(@voter.write_vector.include? [0, ['a', 1]])
+    assert(@voter.write_vector.include?([0, ['a', 1]]))
+  end
+
+  def test_monotonic_reads_sanity
+    p 'test_monotonic_reads_sanity'
+    @voter.init_request <+ [[0, [:MR], [['a', 0]], ['a', 1]]]
+    wait
+    assert(@voter.output_read_result.empty?)
+  end
+
+  def test_read_your_writes_sanity
+    p 'test_read_your_writes_sanity'
+    @voter.init_request <+ [[0, [:RYW], [['a', 0]], ['a', 1]]]
+    wait
+    assert(@voter.output_read_result.empty?)
+  end
+
+  def test_writes_follow_reads_sanity
+    p 'test_writes_follow_reads_sanity'
+    @voter.init_request <+ [[0, [:RYW], [['a', 0]], ['a', 1]]]
+    wait
+    assert(@voter.output_read_result.include?([0, ['a', 0]]))
+  end
+
+  def test_monotonic_writes_sanity
+    p 'test_monotonic_writes_sanity'
+    @voter.init_request <+ [[0, [:MW], [['a', 0]], ['a', 1]]]
+    wait
+    assert(@voter.output_read_result.include?([0, ['a', 1]]))
   end
 
 end
