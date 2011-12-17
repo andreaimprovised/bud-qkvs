ceeee501a9fe1921d2cec7f4d34ec56a2e57748c
Alex Degtiar <alex.degtiar@gmail.com>
2011-12-16 17:24:36 -0800

    Refactoring tests.

--- test/tc_session_voting.rb
+++ test/tc_session_voting.rb
@@ -32,56 +32,6 @@ class TestSessionVoting < Test::Unit::TestCase
     @voter = SessionVoter.new()
   end
 
-  def test_init_guarantees
-    p 'test_init_guarantees'
-    init_dummy_request
-    wait
-    assert(@voter.session_guarantees.include?([0, [:foo, :bar]]))
-  end
-
-  def test_init_read_vectors
-    p 'test_init_read_vectors'
-    init_dummy_request
-    wait
-    assert(@voter.read_vectors.include?([0, [['a', 0]]]))
-    assert(@voter.read_vectors.include?([0, [['b', 1]]]))
-  end
-
-  def test_init_write_vector
-    p 'test_init_write_vectors'
-    init_dummy_request
-    wait
-    assert(@voter.write_vector.include?([0, [['a', 1]]]))
-  end
-
-  def test_monotonic_reads_empty
-    p 'test_monotonic_reads_empty'
-    @voter.init_request <+ [[0, [:MR], [[['a', 0]]], [['a', 1]]]]
-    wait
-    assert(@voter.output_read_result.empty?)
-  end
-
-  def test_read_your_writes_empty
-    p 'test_read_your_writes_empty'
-    @voter.init_request <+ [[0, [:RYW], [[['a', 0]]], [['a', 1]]]]
-    wait
-    assert(@voter.output_read_result.empty?)
-  end
-
-  def test_writes_follow_reads_empty
-    p 'test_writes_follow_reads_empty'
-    @voter.init_request <+ [[0, [:WFR], [[['a', 0]]], [['a', 1]]]]
-    wait
-    assert(@voter.output_write_result.include?([0, [['a', 0]]]))
-  end
-
-  def test_monotonic_writes_empty
-    p 'test_monotonic_writes_empty'
-    @voter.init_request <+ [[0, [:MW], [['a', 0]], [['a', 1]]]]
-    wait
-    assert(@voter.output_write_result.include?([0, [['a', 1]]]))
-  end
-
   def test_read_simple
     p 'test_read_simple'
     @voter.init_request <+ [[0, [], [[]], []]]
@@ -140,6 +90,56 @@ class TestSessionVoting < Test::Unit::TestCase
     @voter.add_write <+ [[0, [['b', 2]]]]
     wait
     assert(@voter.output_write_result.include?([0, [['a', 1], ['b', 2]]]))
+  end
+
+  def test_init_guarantees
+    p 'test_init_guarantees'
+    init_dummy_request
+    wait
+    assert(@voter.session_guarantees.include?([0, [:foo, :bar]]))
+  end
+
+  def test_init_read_vectors
+    p 'test_init_read_vectors'
+    init_dummy_request
+    wait
+    assert(@voter.read_vectors.include?([0, [['a', 0]]]))
+    assert(@voter.read_vectors.include?([0, [['b', 1]]]))
+  end
+
+  def test_init_write_vector
+    p 'test_init_write_vectors'
+    init_dummy_request
+    wait
+    assert(@voter.write_vector.include?([0, [['a', 1]]]))
+  end
+
+  def test_monotonic_reads_empty
+    p 'test_monotonic_reads_empty'
+    @voter.init_request <+ [[0, [:MR], [[['a', 0]]], [['a', 1]]]]
+    wait
+    assert(@voter.output_read_result.empty?)
+  end
+
+  def test_read_your_writes_empty
+    p 'test_read_your_writes_empty'
+    @voter.init_request <+ [[0, [:RYW], [[['a', 0]]], [['a', 1]]]]
+    wait
+    assert(@voter.output_read_result.empty?)
+  end
+
+  def test_writes_follow_reads_empty
+    p 'test_writes_follow_reads_empty'
+    @voter.init_request <+ [[0, [:WFR], [[['a', 0]]], [['a', 1]]]]
+    wait
+    assert(@voter.output_write_result.include?([0, [['a', 0]]]))
+  end
+
+  def test_monotonic_writes_empty
+    p 'test_monotonic_writes_empty'
+    @voter.init_request <+ [[0, [:MW], [['a', 0]], [['a', 1]]]]
+    wait
+    assert(@voter.output_write_result.include?([0, [['a', 1]]]))
   end
 
 end
